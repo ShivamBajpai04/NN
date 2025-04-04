5d27805fef4436c4734157b525b1d466331cd33a
ShivamBajpai04 <shivambajpai04@gmail.com>
2025-04-05 03:37:56 +0530

    Implement XOR neural network with forward propagation, cost calculation, and learning functions

--- NN.c
+++ NN.c
@@ -3,25 +3,207 @@
 #include "NN.h"
 #include <time.h>
 
-float train_input[] = {0, 0, 1, 0, 0, 1, 1, 1};
-float train_output[] = {0, 1, 1, 0};
+typedef struct
+{
+    Mat a0;
+    Mat w1, b1, a1;
+    Mat w2, b2, a2;
+} Xor;
+
+Xor xor_alloc(void)
+{
+    Xor m;
+    m.a0 = mat_alloc(1, 2);
+    m.w1 = mat_alloc(2, 2);
+    m.b1 = mat_alloc(1, 2);
+    m.a1 = mat_alloc(1, 2);
+    m.w2 = mat_alloc(2, 1);
+    m.b2 = mat_alloc(1, 1);
+    m.a2 = mat_alloc(1, 1);
+    return m;
+}
+
+void forward_xor(Xor m)
+{
+    mat_dot(m.a0, m.w1, m.a1); // a1 = x * w1
+    mat_sum(m.a1, m.b1);       // a1 = a1 + b1
+    mat_sigmoid(m.a1);         // a1 = sigmoid(a1)
+
+    mat_dot(m.a1, m.w2, m.a2); // a2 = a1 * w2
+    mat_sum(m.a2, m.b2);       // a2 = a2 + b2
+    mat_sigmoid(m.a2);         // a2 = sigmoid(a2)
+}
+
+float cost(Xor m, Mat train_input, Mat train_output)
+{
+    assert(train_input.rows == train_output.rows);
+    assert(train_output.cols == m.a2.cols);
+    size_t n = train_input.rows;
+    float cost = 0;
+    for (size_t i = 0; i < n; ++i)
+    {
+        Mat x = mat_row(train_input, i), y = mat_row(train_output, i);
+
+        mat_copy(m.a0, x);
+        forward_xor(m);
+
+        size_t cols = train_output.cols;
+        for (size_t j = 0; j < cols; ++j)
+        {
+            float d = MAT_AT(m.a2, 0, j) - MAT_AT(y, 0, j);
+            cost += d * d;
+        }
+    }
+    return cost / n;
+}
+
+void finite_diff(Xor m, Xor g, float eps, Mat ti, Mat to)
+{
+    float saved;
+    float c = cost(m, ti, to);
+    for (size_t i = 0; i < m.w1.rows; ++i)
+    {
+        for (size_t j = 0; j < m.w1.cols; ++j)
+        {
+            saved = MAT_AT(m.w1, i, j);
+            MAT_AT(m.w1, i, j) += eps;
+            MAT_AT(g.w1, i, j) = (cost(m, ti, to) - c) / eps;
+            MAT_AT(m.w1, i, j) = saved;
+        }
+    }
+
+    for (size_t i = 0; i < m.b1.rows; ++i)
+    {
+        for (size_t j = 0; j < m.b1.cols; ++j)
+        {
+            saved = MAT_AT(m.b1, i, j);
+            MAT_AT(m.b1, i, j) += eps;
+            MAT_AT(g.b1, i, j) = (cost(m, ti, to) - c) / eps;
+            MAT_AT(m.b1, i, j) = saved;
+        }
+    }
+
+    for (size_t i = 0; i < m.w2.rows; ++i)
+    {
+        for (size_t j = 0; j < m.w2.cols; ++j)
+        {
+            saved = MAT_AT(m.w2, i, j);
+            MAT_AT(m.w2, i, j) += eps;
+            MAT_AT(g.w2, i, j) = (cost(m, ti, to) - c) / eps;
+            MAT_AT(m.w2, i, j) = saved;
+        }
+    }
+
+    for (size_t i = 0; i < m.b2.rows; ++i)
+    {
+        for (size_t j = 0; j < m.b2.cols; ++j)
+        {
+            saved = MAT_AT(m.b2, i, j);
+            MAT_AT(m.b2, i, j) += eps;
+            MAT_AT(g.b2, i, j) = (cost(m, ti, to) - c) / eps;
+            MAT_AT(m.b2, i, j) = saved;
+        }
+    }
+}
+
+void xor_learn(Xor m, Xor g, float rate)
+{
+    for (size_t i = 0; i < m.w1.rows; ++i)
+    {
+        for (size_t j = 0; j < m.w1.cols; ++j)
+        {
+            MAT_AT(m.w1, i, j) -= rate * MAT_AT(g.w1, i, j);
+        }
+    }
+
+    for (size_t i = 0; i < m.b1.rows; ++i)
+    {
+        for (size_t j = 0; j < m.b1.cols; ++j)
+        {
+            MAT_AT(m.b1, i, j) -= rate * MAT_AT(g.b1, i, j);
+        }
+    }
+
+    for (size_t i = 0; i < m.w2.rows; ++i)
+    {
+        for (size_t j = 0; j < m.w2.cols; ++j)
+        {
+            MAT_AT(m.w2, i, j) -= rate * MAT_AT(g.w2, i, j);
+        }
+    }
+
+    for (size_t i = 0; i < m.b2.rows; ++i)
+    {
+        for (size_t j = 0; j < m.b2.cols; ++j)
+        {
+            MAT_AT(m.b2, i, j) -= rate * MAT_AT(g.b2, i, j);
+        }
+    }
+}
+
+float td[] = {
+    0, 0, 0,
+    0, 1, 1,
+    1, 0, 1,
+    1, 1, 0};
 
 int main()
 {
+    size_t arch[] = {2, 2, 1};
+    NN nn = nn_alloc(arch, ARRAY_LEN(arch));
+    NN_PRINT(nn);
+
+    return 0;
     srand(time(0));
-    Mat w1 = mat_alloc(2, 2), b1 = mat_alloc(1, 2); // one layer with 2 neurons
-    Mat w2 = mat_alloc(2, 1), b2 = mat_alloc(1, 1); // one layer with 1 neurons
 
-    mat_rand(w1, 0, 10);
-    mat_rand(b1, 0, 10);
-    mat_rand(w2, 0, 10);
-    mat_rand(b2, 0, 10);
+    size_t stride = 3;
+    size_t n = sizeof(td) / sizeof(td[0]) / 3;
+    Mat ti = {
+        .rows = n,
+        .cols = 2,
+        .stride = stride,
+        .data = td,
+    };
+    Mat to = {
+        .rows = n,
+        .cols = 1,
+        .stride = stride,
+        .data = td + 2,
+    };
 
-    Mat input = {.rows = 4, .cols = 2, .data = train_input};
-    Mat output = {.rows = 1, .cols = 4, .data = train_output};
+    MAT_PRINT(ti);
+    MAT_PRINT(to);
 
-    MAT_PRINT(w1);
-    MAT_PRINT(b1);
-    MAT_PRINT(w2);
-    MAT_PRINT(b2);
+    Xor m = xor_alloc();
+    Xor g = xor_alloc();
+
+    mat_rand(m.w1, 0, 1);
+    mat_rand(m.b1, 0, 1);
+    mat_rand(m.w2, 0, 1);
+    mat_rand(m.b2, 0, 1);
+
+    float eps = 1e-1;
+    float rate = 1e-1;
+
+    printf("cost = %f\n", cost(m, ti, to));
+    for (size_t i = 0; i < 1000; ++i)
+    {
+        finite_diff(m, g, eps, ti, to);
+        xor_learn(m, g, rate);
+        // printf("iteration %zu, cost = %f\n", i, cost(m, ti, to));
+    }
+
+    printf("cost = %f\n", cost(m, ti, to));
+
+    for (size_t i = 0; i < 2; ++i)
+    {
+        for (size_t j = 0; j < 2; ++j)
+        {
+            MAT_AT(m.a0, 0, 0) = i;
+            MAT_AT(m.a0, 0, 1) = j;
+            forward_xor(m);
+            float y = *m.a2.data;
+            printf("%zu ^ %zu = %f\n", i, j, y);
+        }
+    }
 }
--- NN.h
+++ NN.h
@@ -15,33 +15,130 @@
 
 #endif // NN_ASSERT
 
+#define ARRAY_LEN(xs) sizeof((xs)) / sizeof((xs)[0])
+
+/**
+ * @brief Generates a random float between 0 and 1.
+ * @return Random float value.
+ */
+float rand_float(void);
+
+/**
+ * @brief Computes the sigmoid function for a given input.
+ * @param x Input value.
+ * @return Sigmoid of the input.
+ */
+float sigmoidf(float x);
+
 typedef struct
 {
     size_t rows;
     size_t cols;
-    // size_t stride;
+    size_t stride;
     float *data;
 } Mat;
 
-#define MAT_AT(m, i, j) (m).data[(i) * (m).cols + (j)]
+#define MAT_AT(m, i, j) (m).data[(i) * (m).stride + (j)]
 
-float sigmoidf(float x);
-float rand_float(void);
+/**
+ * @brief Allocates memory for a matrix with the specified dimensions.
+ * @param rows Number of rows in the matrix.
+ * @param cols Number of columns in the matrix.
+ * @return Allocated matrix.
+ */
+Mat mat_alloc(size_t rows, size_t cols);
 
-Mat mat_alloc(size_t rows, size_t cols); // allocate memory to the matrix dynamically
-void mat_fill(Mat m, float val);         // fill the matrix with val
-void mat_rand(Mat m, float s, float e);  // randomize the matrixx
-void mat_dot(Mat a, Mat b, Mat dest);    // dest is output, and a and b are operands; No memory allocation outside of mat_alloc()
-void mat_sum(Mat dest, Mat b);           // add two matrices
-void mat_print(Mat m, const char *name); // print the contents of the matrix
+/**
+ * @brief Fills a matrix with a specified value.
+ * @param m Matrix to fill.
+ * @param val Value to fill the matrix with.
+ */
+void mat_fill(Mat m, float val);
+
+/**
+ * @brief Randomizes the elements of a matrix within a specified range.
+ * @param m Matrix to randomize.
+ * @param s Start of the range.
+ * @param e End of the range.
+ */
+void mat_rand(Mat m, float s, float e);
+
+/**
+ * @brief Extracts a specific row from a matrix as a new matrix.
+ * @param m Source matrix.
+ * @param row Index of the row to extract.
+ * @return A matrix representing the specified row.
+ */
+Mat mat_row(Mat m, size_t row);
+
+/**
+ * @brief Copies the contents of one matrix to another.
+ * @param dest Destination matrix.
+ * @param src Source matrix.
+ */
+void mat_copy(Mat dest, Mat src);
+
+/**
+ * @brief Computes the dot product of two matrices and stores the result in a destination matrix.
+ * @param a First operand matrix.
+ * @param b Second operand matrix.
+ * @param dest Destination matrix to store the result.
+ */
+void mat_dot(Mat a, Mat b, Mat dest);
+
+/**
+ * @brief Adds two matrices element-wise and stores the result in the destination matrix.
+ * @param dest Destination matrix to store the result.
+ * @param b Matrix to add to the destination matrix.
+ */
+void mat_sum(Mat dest, Mat b);
+
+/**
+ * @brief Prints the contents of a matrix to the console.
+ * @param m Matrix to print.
+ * @param name Name of the matrix (for display purposes).
+ */
+void mat_print(Mat m, const char *name, size_t padding);
+
+/**
+ * @brief Applies the sigmoid function to each element of a matrix.
+ * @param m Matrix to apply the sigmoid function to.
+ */
 void mat_sigmoid(Mat m);
-#define MAT_PRINT(m) mat_print(m, #m)
+
+#define MAT_PRINT(m) mat_print(m, #m, 0)
+
+typedef struct
+{
+    size_t count;
+    Mat *ws;
+    Mat *bs;
+    Mat *as; // The amount of activations is count + 1
+} NN;
+
+/**
+ * @brief Allocates memory for a neural network based on the given architecture.
+ * @param arch Array representing the number of neurons in each layer.
+ * @param arch_count Number of layers in the architecture.
+ * @return Allocated neural network.
+ */
+NN nn_alloc(size_t *arch, size_t arch_count);
+
+/**
+ * @brief Prints the details of a neural network, including weights and biases.
+ * @param nn Neural network to print.
+ * @param name Name of the neural network (for display purposes).
+ */
+void nn_print(NN nn, const char *name);
+
+#define NN_PRINT(nn) nn_print(nn, #nn)
+
 #endif // NN_H_
 
 #ifdef NN_IMPLEMENTATION
 
 float sigmoidf(float x)
-{   
+{
     return 1.f / (1.f + expf(-x));
 }
 
@@ -55,7 +152,8 @@ Mat mat_alloc(size_t rows, size_t cols)
     Mat m;
     m.rows = rows;
     m.cols = cols;
-    m.data = malloc(sizeof(*m.data) * rows * cols);
+    m.stride = cols;
+    m.data = (float *)malloc(sizeof(*m.data) * rows * cols);
     NN_ASSERT(m.data != NULL);
     return m;
 }
@@ -90,18 +188,19 @@ void mat_sum(Mat dest, Mat b)
         }
     }
 }
-void mat_print(Mat m, const char *name)
+void mat_print(Mat m, const char *name, size_t padding)
 {
-    printf("%s : [\n", name);
+    printf("%*s%s = [\n", (int)padding, "", name);
     for (size_t i = 0; i < m.rows; ++i)
     {
+        printf("%*s    ", (int)padding, "");
         for (size_t j = 0; j < m.cols; ++j)
         {
-            printf("    %f ", MAT_AT(m, i, j));
+            printf("%f ", MAT_AT(m, i, j));
         }
         printf("\n");
     }
-    printf("]\n");
+    printf("%*s]\n", (int)padding, "");
 }
 
 void mat_rand(Mat m, float s, float e)
@@ -126,13 +225,78 @@ void mat_fill(Mat m, float val)
     }
 }
 
-void mat_sigmoid(Mat m){
+void mat_sigmoid(Mat m)
+{
     for (size_t i = 0; i < m.rows; ++i)
     {
         for (size_t j = 0; j < m.cols; ++j)
         {
-            MAT_AT(m, i, j) = sigmoidf(MAT_AT(m,i,j));
+            MAT_AT(m, i, j) = sigmoidf(MAT_AT(m, i, j));
         }
     }
 }
+
+Mat mat_row(Mat m, size_t row)
+{
+    return (Mat){
+        .rows = 1,
+        .cols = m.cols,
+        .stride = m.stride,
+        .data = &MAT_AT(m, row, 0),
+    };
+}
+
+void mat_copy(Mat dest, Mat src)
+{
+    NN_ASSERT(dest.rows == src.rows);
+    NN_ASSERT(dest.cols == src.cols);
+    for (size_t i = 0; i < dest.rows; ++i)
+    {
+        for (size_t j = 0; j < dest.cols; ++j)
+        {
+            MAT_AT(dest, i, j) = MAT_AT(src, i, j);
+        }
+    }
+}
+
+// size_t arch[] = {2,2,1};
+// NN nn = nn_alloc(arch,ARRAY_LEN(arch));
+
+NN nn_alloc(size_t *arch, size_t arch_count)
+{
+    NN_ASSERT(arch_count > 0);
+    NN nn;
+    nn.count = arch_count - 1;
+
+    nn.ws = NN_MALLOC(sizeof(*nn.ws) * nn.count);
+    NN_ASSERT(nn.ws != NULL);
+    nn.bs = NN_MALLOC(sizeof(*nn.bs) * nn.count);
+    NN_ASSERT(nn.bs != NULL);
+    nn.as = NN_MALLOC(sizeof(*nn.as) * nn.count + 1);
+    NN_ASSERT(nn.as != NULL);
+
+    nn.as[0] = mat_alloc(1, arch[0]);
+    for (size_t i = 1; i < arch_count; ++i)
+    {
+        nn.ws[i - 1] = mat_alloc(nn.as[i - 1].cols, arch[i]);
+        nn.bs[i - 1] = mat_alloc(1, arch[i]);
+        nn.as[i] = mat_alloc(1, arch[i]);
+    }
+    return nn;
+}
+
+void nn_print(NN nn, const char *name)
+{
+    char buf[256];
+    printf("%s = [\n", name);
+    for (size_t i = 0; i < nn.count; ++i)
+    {
+        snprintf(buf, sizeof(buf), "ws[%zu]", i);
+        mat_print(nn.ws[i], buf, 4);
+        snprintf(buf, sizeof(buf), "bs[%zu]", i);
+        mat_print(nn.bs[i], buf, 4);
+    }
+    printf("]\n");
+}
+
 #endif // NN_IMPLEMENTATION
